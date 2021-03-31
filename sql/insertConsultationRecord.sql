1ceb460769df5927677bfae1799b0c3931ce2c91
wingc <wingchung16@gmail.com>
2021-03-31 22:17:31 +0800

    Added insert data sql

---
+++ sql/insertConsultationRecord.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM codetestDB.consultationrecord
+LIMIT 0, 1000
+
+-- Date: 2021-03-31 21:49
+*/
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (1,'ken','amy','dfgs','dfsgfd',100,'2021-05-05 15:00:00',0,7);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (2,'xiu','bee','sbdfb','bvbc',200,'2021-04-05 12:00:00',1,7);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (3,'eee','tee','svsd','dfgsgs',200,'2021-03-22 12:00:00',1,7);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (5,'eee','tee','qqq','wwww',500,'2021-03-22 15:00:00',1,19);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (6,'aaa','bbb','gfdgsd','fdgsdg',150,'2021-03-30 15:00:00',0,19);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (7,'qq','gdfg','bdgh','dsghdhgh123',250,'2021-03-19 15:00:00',1,19);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (8,'ffg','bbb','gfdgsd','fdgsdg',150,'2021-03-30 11:00:00',0,19);
+INSERT INTO `consultationrecord` (`recordId`,`doctorname`,`patientname`,`diagnosis`,`medication`,`consultationfee`,`datetime`,`followup`,`userid`) VALUES (9,'ema','tee','qqq','wwww',500,'2021-03-22 15:00:00',1,19);
---
+++ sql/insertUser.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM codetestDB.users
+LIMIT 0, 1000
+
+-- Date: 2021-03-31 21:50
+*/
+INSERT INTO `users` (`userid`,`email`,`password`,`clinicname`,`phonenumber`,`address`) VALUES (7,'Qwe@qwe.com','$2a$04$bfptF5F2NvP7HI81IltAzuwgqIkiUCbjKJRuKHmA1Fexhk9KueJYy','Qwe','87654321','Sfhhgdc');
+INSERT INTO `users` (`userid`,`email`,`password`,`clinicname`,`phonenumber`,`address`) VALUES (18,'123@123.com','$2a$04$YPEEKIOv1Qcl4yBok7eyu.r73BSgHmijlHaG7Oxz5RnQVkl0lZGXm','Gfdc','54326543','Bfdgjx');
+INSERT INTO `users` (`userid`,`email`,`password`,`clinicname`,`phonenumber`,`address`) VALUES (19,'amy@amy.com','$2a$04$bfptF5F2NvP7HI81IltAzuwgqIkiUCbjKJRuKHmA1Fexhk9KueJYy','erwwe','65324232','hdfhgh');
+INSERT INTO `users` (`userid`,`email`,`password`,`clinicname`,`phonenumber`,`address`) VALUES (20,'Asd@123.com','$2a$04$2XJ1IPXIlMIxSHds1aC06ObDxUPsKZ.WQiz6PvpyYdzAQT/vUcQAu','Asd','24747272','Jejdciucdb');
